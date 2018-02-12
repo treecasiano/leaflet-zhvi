aa221bf61238c0557d2b179d7fa9dc7e8815fbd7
Tree Casiano <tree.pdx@gmail.com>
2018-02-11 17:34:25 -0800

    Add display text formatting for month of year

--- js/geojson.js
+++ js/geojson.js
@@ -1,21 +1,19 @@
 function getMap(){
 
     var myMap;
-    var geojsonMarkerOptions;
-    
     // default values
-    var myCenterCoords = [39.8097, -98.5556];  
-    var defaultZoom = 4
-    
+    var myCenterCoords = [39.8097, -98.5556];
+    var defaultZoom = 4;
+
     var tileLayerUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var tileLayerAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
     // create leaflet objects
     myMap = L.map('map-id').setView(myCenterCoords, defaultZoom);
-        
+
 	L.tileLayer(tileLayerUrl, {
 		attribution: tileLayerAttrib,
-		maxZoom: 18,
+		maxZoom: 18
 
 	}).addTo(myMap);
 
@@ -26,15 +24,15 @@ function getMap(){
         $.ajax('data/metroRegionsZHVI2017.geojson', {
             dataType: 'json',
             success: function(response) {
-                
+
                 var geojsonLayer = L.geoJson(response, {
                     pointToLayer: pointToLayer
                 });
 
                 map.addLayer(geojsonLayer);
-    
+
             }
-        });  
+        });
     }
 
     function pointToLayer(feature, latlng) {
@@ -44,17 +42,18 @@ function getMap(){
             radius: 5,
             fillColor: "#8B008B",
             color: "#000",
-            weight: 1, 
-            opacity: 1, 
+            weight: 1,
+            opacity: 1,
             fillOpacity: 0.9
-        }     
+        };
 
         geojsonMarkerOptions.radius = calculateSymbolRadius(attributeValue);
 
         var layer = L.circleMarker(latlng, geojsonMarkerOptions);
 
         var cityDisplayName = "<p><strong>City:</strong> " + feature.properties.regionName + "</p>";
-        var attributeDisplayText = "<p><strong>Attribute:</strong> " + feature.properties[attribute] + "</p>";
+        var attributeDisplayText = "<p><strong>Median Home Value " +
+            formatMonth(attribute)  + "&nbsp;2017: </strong>" + feature.properties[attribute] + "</p>";
         var popupContent = cityDisplayName + attributeDisplayText;
         layer.bindPopup(popupContent);
         return layer;
@@ -62,11 +61,29 @@ function getMap(){
 
     function calculateSymbolRadius(attrValue) {
         var scaleFactor = .0006;
-        var area = attrValue * scaleFactor; 
+        var area = attrValue * scaleFactor;
         var radius = Math.sqrt(area/Math.PI);
         return radius;
     }
 
+    function formatMonth(zhviAttr) {
+        var monthsOfYear = {
+           '01': 'January',
+           '02': 'February',
+           '03': 'March',
+           '04': 'April',
+           '05': 'May',
+           '06': 'June',
+           '07': 'July',
+           '08': 'August',
+           '09': 'September',
+           '10': 'October',
+           '11': 'November',
+           '12': 'December'
+        };
+        var monthString = zhviAttr.slice(-2);
+        return monthsOfYear[monthString];
+    }
 }
 
 $(document).ready(getMap);
