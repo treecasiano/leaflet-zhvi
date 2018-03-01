bc5b58838391917008252ac36e5f2ea987cf8e0c
Tree Casiano <tree.pdx@gmail.com>
2018-02-28 20:28:19 -0800

    Add dragging functionality to info panel

--- js/main.js
+++ js/main.js
@@ -184,7 +184,7 @@ function getMap(){
 
         map.addControl(new LegendControl());
         $('#info-button').click(function() {
-            infoPanel.fadeToggle('slow');
+            infoPanel.toggle();
         });
     }
 
@@ -414,5 +414,57 @@ function getMap(){
         return cityDisplayName + label + homeValue;
     }
 }
+
+
+// The code below is an adaptation from https://codepen.io/chriscoyier/pen/zdsty
+// and https://github.com/pjfsilva/dte-project/blob/master/jquery-draggable/draggable.js
+
+(function($) {
+    $.fn.drags = function(opt) {
+
+        opt = $.extend({handle: '', cursor:"move"}, opt);
+
+        if (opt.handle === '') {
+            var $el = this;
+        } else {
+            var $el = this.find(opt.handle);
+        }
+
+        return $el.css('cursor', opt.cursor)
+            .on('mousedown touchstart', function(e) {
+                if (opt.handle === "") {
+                    var $drag = $(this).addClass('draggable');
+                } else {
+                    var $drag = $(this).addClass('active-handle').parent().addClass('draggable');
+                }
+                var z_idx = $drag.css('z-index'),
+                    drg_h = $drag.outerHeight(),
+                    drg_w = $drag.outerWidth(),
+                    pos_y = $drag.offset().top + drg_h - e.pageY,
+                    pos_x = $drag.offset().left + drg_w - e.pageX;
+
+                $drag.css('z-index', 1000).parents()
+                    .on("mousemove touchmove", function(e) {
+                        console.log("yo this is being touched!");
+                        $('.draggable').offset({
+                        top:e.pageY + pos_y - drg_h,
+                        left:e.pageX + pos_x - drg_w
+                    }).on("mouseup touchend", function() {
+                        $(this).removeClass('draggable').css('z-index', z_idx);
+                    });
+                });
+            e.preventDefault();
+        }).on("mouseup touchend", function() {
+            if (opt.handle === "") {
+                $(this).parent().removeClass('draggable');
+            } else {
+                $(this).removeClass('active-handle').parent().removeClass('draggable');
+            }
+        });
+
+    }
+})($);
+
+$('#panel').drags({handle: '.draggable-handler', cursor: 'move'});
 
 $(document).ready(getMap);
